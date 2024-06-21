3732ad70bf99d5e85890f825ca108d467adff881
Pascal SAN <54542636+Pascalo02@users.noreply.github.com>
2024-06-21 22:42:16 +0200

    fix: Render index.html server side

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 def login(request):
-    html = render_to_string('login.html', request=request)
+    html = render_to_string('accounts/login.html', request=request)
     return HttpResponse(html)
 
 def register(request):
-    html = render_to_string('register.html', request=request)
+    html = render_to_string('accounts/register.html', request=request)
     return HttpResponse(html)
