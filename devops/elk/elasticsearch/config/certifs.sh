26c3d3bc9666761413dc6f63ed6d050676ac0a31
cloosafm <alexandra.cloos@gmail.com>
2024-07-08 15:34:30 +0200

    getting there with identification... maybe

--- devops/elk/elasticsearch/config/certifs.sh
+++ devops/elk/elasticsearch/config/certifs.sh
@@ -1,51 +1,35 @@
 #! /bin/bash
 
-    #    if [ x${ELASTIC_PASSWORD} == x ]; then
-    #      echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
-    #      exit 1;
-    #    elif [ x${KIBANA_SYSTEM_PASSWORD} == x ]; then
-    #      echo "Set the KIBANA_PASSWORD environment variable in the .env file";
-    #      exit 1;
-    #    fi;
-    #    if [ ! -f config/certs/ca.zip ]; then
-    #      echo "Creating CA";
-    #      bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
-    #      unzip config/certs/ca.zip -d config/certs;
-    #    fi;
-    #    if [ ! -f config/certs/certs.zip ]; then
-    #      echo "Creating certs";
-    #      echo -ne \
-    #      "instances:\n"\
-    #      "  - name: elasticsearch\n"\
-    #      "    dns:\n"\
-    #      "      - elasticsearch\n"\
-    #      "      - localhost\n"\
-    #      "    ip:\n"\
-    #      "      - 127.0.0.1\n"\
-    #      "  - name: kibana\n"\
-    #      "    dns:\n"\
-    #      "      - kibana\n"\
-    #      "      - localhost\n"\
-    #      "    ip:\n"\
-    #      "      - 127.0.0.1\n"\
-    #      > config/certs/instances.yml;
-    #      bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
-    #      unzip config/certs/certs.zip -d config/certs;
-    #    fi;
-    #    echo "Setting file permissions"
-    #    chown -R root:root config/certs;
-    #    find . -type d -exec chmod 750 \{\} \;;
-    #    find . -type f -exec chmod 640 \{\} \;;
-    #    echo "Waiting for Elasticsearch availability";
-    #    until curl -s --cacert config/certs/ca/ca.crt https://localhost:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
-    #    echo "Setting kibana_system password";
-    #    until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://localhost:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_SYSTEM_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
-    #    echo "All done!";
-
-
 
 # Ensure the script exits on any error
-set -e
+# set -e
+
+
+# Set variables
+set timeout -1
+set elastic_user "elastic"
+set new_password ${ELASTIC_PASSWORD}
+
+
+# expect <<EOF
+# set timeout -1
+# spawn /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -i
+# expect "Continue with password reset? \[y/N\]"
+# send -- "y\r"
+# expect "Enter new password for the elastic user: "
+# send -- "${ELASTIC_PASSWORD}\r"
+# expect "Re-enter new password for the elastic user: "
+# send -- "${ELASTIC_PASSWORD}\r"
+# expect eof
+# EOF
+
+## confirm the password was reset:
+#### Password for the [elastic] user successfully reset.
+
+
+
+
+
 
 # Environment variable for the certificate password
 # Make sure to set this environment variable before running the script
@@ -82,10 +66,8 @@ echo "$ELASTIC_CERT_PWD" | ./bin/elasticsearch-keystore add xpack.security.trans
 
 
 
+
 cd /usr/share/elasticsearch/bin
 ./elasticsearch
 
-# ./bin/elasticsearch
 
-
-echo "Elasticsearch certificates setup completed."
