b2c00a60ee6275a78f9c6c65ca6092b7bc813c41
Pascal SAN <54542636+Pascalo02@users.noreply.github.com>
2024-07-31 17:18:19 +0200

    fix: Reformat html pages

--- web/django/static/js/main.js
+++ web/django/static/js/main.js
@@ -262,6 +262,38 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
             }
         }
+
+        var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
+        for (let toastTrigger of toastTriggers) {
+            toastTrigger.addEventListener('click', function () {
+                var toastSelector = toastTrigger.getAttribute('data-bs-target');
+                if (!toastSelector) return;
+                try {
+                    var toastEl = document.querySelector(toastSelector);
+                    if (!toastEl) return;
+                    var toast = new bootstrap.Toast(toastEl);
+                    toast.show();
+                }
+                catch(e) {
+                    console.error(e);
+                }
+            })
+        }
+
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        })
+
+        if (window.innerWidth < 768) {
+            [].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+                elem.classList.remove('animated');
+                elem.removeAttribute('data-bss-hover-animate');
+                elem.removeAttribute('data-aos');
+                elem.removeAttribute('data-bss-parallax-bg');
+                elem.removeAttribute('data-bss-scroll-zoom');
+            });
+        }
     };
     
     window.addEventListener('popstate', renderPage);
