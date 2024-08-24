ed8eb22e0586517ccb9bb0ee456f306bb9aded88
Pascal SAN <54542636+Pascalo02@users.noreply.github.com>
2024-08-24 11:03:34 +0200

    fix: load bootstrap methods after

---
+++ web/django/static/js/bs-init.js
@@ -0,0 +1,19 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+		elem.removeAttribute('data-bss-parallax-bg');
+		elem.removeAttribute('data-bss-scroll-zoom');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+	const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl,  {trigger: 'hover'}));
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+		return new bootstrap.Popover(popoverTriggerEl)
+	})
+}, false);
--- web/django/static/js/main.js
+++ web/django/static/js/main.js
@@ -426,23 +426,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })
         }
-
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl,  {trigger: 'hover'}));
-        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-            return new bootstrap.Popover(popoverTriggerEl)
-        })
-
-        if (window.innerWidth < 768) {
-            [].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-                elem.classList.remove('animated');
-                elem.removeAttribute('data-bss-hover-animate');
-                elem.removeAttribute('data-aos');
-                elem.removeAttribute('data-bss-parallax-bg');
-                elem.removeAttribute('data-bss-scroll-zoom');
-            });
-        }
     };
 
     const setNotifHandlers = () => {
